fcc452f010f218e2623f0dfa4eb87a9810ffa5ff
tuan_anh <tuananh19062010comboa123@gmail.com>
2017-04-02 15:19:11 +0700

    add mutil Model

--- src/Model/Item.java
+++ src/Model/Item.java
@@ -1,5 +1,20 @@
 package Model;
 
 public class Item {
-
+ private String name;
+ private Price price;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public Price getPrice() {
+	return price;
+}
+public void setPrice(Price price) {
+	this.price = price;
+}
+ 
+ 
 }
